0a99540187ea2344c4d7d21d566e5eb9250fd7e8
jady <jaime.delao@alumnos.udg.mx>
2016-09-29 19:23:48 -0500

    comentario dos

---
+++ circulo.py
@@ -0,0 +1,8 @@
+print("Calcular area de un circulo")
+ladoA = input("Dame el radio de un circulo: ")
+
+ladoA_int = int(ladoA)
+
+area = 3.141592 * ladoA_int**2
+print("El area del cuadrado es")
+print(area)
